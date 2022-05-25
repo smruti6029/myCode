71123562f528a33261bb0e8180f55bb47a0e3713
smruti <smrutiranjannayak6029@gmail.com>
2022-05-25 09:09:59 +0530

    pyy fff

--- Practice/New folder/py1.py
+++ Practice/New folder/py1.py
@@ -1,3 +1,7 @@
+from defusedxml import DTDForbidden
+
+
 def func():
     print("hello world")
 func()
+DTDForbiddendddf
---
+++ Practice/mas.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main()
+{
+    int a=5,b=6;
+    int c;
+    c=a+b;
+    printf("%d",c);
+}
